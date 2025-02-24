c692c736d7f457635889773b4667d0ba48826e4a
Joat <michaelpaterson194@gmail.com>
2025-02-24 22:17:25 +0000

    API && Database Dev:
    Created an API for the email sender webpage with Express.
    Initialized a MongoDB database to store Keywords and sent emails.
    Connect to the Database with Mongoose from the config folder.
    Added a gitignore file and installed necessary dependencies such as nodemon for launching server.

---
+++ server.mjs
@@ -0,0 +1,25 @@
+import express from 'express';
+import 'dotenv/config';
+import connectDB from './config/database.mjs';
+import mongoose from 'mongoose';
+
+// Connect to the MongoDB database
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Email Security');
+});
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connected...');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}...`);
+    });
+});
